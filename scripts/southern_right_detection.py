1aec0f5dcbd317a985617e6ee8497c4a063f8c9f
James Perks <jamesfperks@aol.com>
2023-11-05 18:17:14 +0000

    code refactoring

--- scripts/southern_right_detection.py
+++ scripts/southern_right_detection.py
@@ -110,31 +110,31 @@ def print_files(fileset):
 def extract_samples(fileset):
     """Extract feature vector X and labels y from a given fileset"""
     
+    # Get sections from all recordings
+    whale_sections, noise_sections = get_class_sections(fileset)
     
-    """
-    Test files results:
-        
-    Using slice_data to generate frames gives
-    Whale frames: (505, 800)
-    Noise frames: (6430, 800)
     
-    Feature vector Should have same no. of frames but with row size of N_MFCC.
-    Instead, gives:
-        
-    Whale features: (541, 3)
-    Noise features: (6466, 3)
+    # Extract features
+    whale_features, noise_features = get_class_features(whale_sections,
+                                                        noise_sections)
     
-    CHECK WHY THIS IS...
     
-    CHECK THAT ZERO SECTION DATA IS FIXED...
-    IMPORTANT THIS MAY HAVE CHANGED THE ORDER OF WHALE/NOISE SECTIONS
-    THE ZERO ARRAY IN TRACK 7 APPEARS IN THE NOISE SECTIONS....
-    i.e. the fact that some sections are empty lists [] due to dodgy
-    log values does not matter
+    # Balance dataset
+    #noise_features = balance_dataset(whale_features, noise_features)
     
-    """
     
-    # Combine sections from all recordings
+    # Label samples
+    labelled_samples = label_samples(whale_features, noise_features)
+    
+    # Collect and randomise sample set
+    X, y = get_sample_vectors(labelled_samples)
+
+    return X, y
+
+
+def get_class_sections(fileset):
+    """Get all whale and noise sections from all recordings"""
+    
     all_whale_sections = []
     all_noise_sections = []
     
@@ -144,56 +144,8 @@ def extract_samples(fileset):
         
         all_whale_sections.extend(whale_sections)
         all_noise_sections.extend(noise_sections)
-      
     
-    # Extract features
-    all_whale_features = np.vstack(tuple(extract_features(section) for
-                                         section in all_whale_sections
-                                         if section.size > 0))
-    
-    all_noise_features = np.vstack(tuple(extract_features(section) for
-                                         section in all_noise_sections
-                                         if section.size > 0))
-    
-    
-    #all_whale_frames = np.vstack(tuple(slice_data(section) for section in all_whale_sections if section.size > 0))
-    #all_noise_frames = np.vstack(tuple(slice_data(section) for section in all_noise_sections if section.size > 0))
-    
-    #print(all_whale_features.shape)
-    #print(all_noise_features.shape)
-    #print(all_whale_frames.shape)
-    #print(all_noise_frames.shape)
-    
-    labelled_whale_features = np.c_[all_whale_features, np.ones(all_whale_features.shape[0])]
-    labelled_noise_features = np.c_[all_noise_features, np.zeros(all_noise_features.shape[0])]
-    
-    all_samples = np.vstack((labelled_whale_features, labelled_noise_features))
-    
-    np.random.shuffle(all_samples)
-    
-    X = all_samples[:,0:N_MFCC]
-    
-    y = all_samples[:,-1]
-
-    
-    """
-    LEGACY
-    # Format: [(["whale1"], ["noise1"]), (["whale2"], ["noise2"]), ...]
-    all_frames = [separate_class_data(log_filename, wav_filename) for
-                  log_filename, wav_filename in fileset]
-    
-    # Format: [(["whale1"], ["whale2"], ...), (["noise1"], ["noise2"], ...)]
-    all_frames_by_class = list(zip(*all_frames))
-    
-    
-    all_whale_frames = np.vstack(all_frames_by_class[0])
-    all_noise_frames = np.vstack(all_frames_by_class[1])
-    
-    print(all_whale_frames.shape)
-    print(all_noise_frames.shape)
-    """
-    
-    return X, y
+    return all_whale_sections, all_noise_sections
 
 
 def separate_class_data(log_filename, wav_filename):
@@ -206,10 +158,6 @@ def separate_class_data(log_filename, wav_filename):
     # Separate into "whale" and "noise" sections
     time_log_indexes = time2index(time_log)
     whale_sections, noise_sections = extract_labelled_sections(y, time_log_indexes)
-    
-    # Create "whale" and "noise" frames (legacy)
-    #whale_frames = generate_frames(whale_sections)
-    #noise_frames = generate_frames(noise_sections)
     
     return whale_sections, noise_sections
 
@@ -229,6 +177,20 @@ def extract_labelled_sections(y, time_log_indexes):
     return whale_sections, noise_sections
 
 
+def get_class_features(whale_sections, noise_sections):
+    """Compute the feature vectors for each class."""
+    
+    whale_features = np.vstack(tuple(extract_features(section) for
+                                         section in whale_sections
+                                         if section.size > 0))
+    
+    noise_features = np.vstack(tuple(extract_features(section) for
+                                         section in noise_sections
+                                         if section.size > 0))
+    
+    return whale_features, noise_features
+
+
 def extract_features(data):
     """Extarct feature vector given a 1D array of data"""
     
@@ -243,6 +205,42 @@ def calculate_mfccs(y):
                                  hop_length=HOP_LENGTH, n_mels=N_MELS))
     
     return mfccs
+
+
+def balance_dataset(minor_class, major_class):
+    """Sub-sample the majority class to balance the dataset."""
+    
+    
+    
+    return None
+
+
+def label_samples(whale_features, noise_features):
+    """Label sample set. Whale = 1. Noise = 0."""
+    
+    labelled_whale_features = np.c_[whale_features,
+                                    np.ones(whale_features.shape[0])]
+    
+    labelled_noise_features = np.c_[noise_features,
+                                    np.zeros(noise_features.shape[0])]
+    
+    labelled_samples = np.vstack((labelled_whale_features,
+                                  labelled_noise_features))
+    
+    return labelled_samples
+
+
+def get_sample_vectors(samples):
+    """Create sample vectors X, y."""
+    
+    np.random.shuffle(samples)
+    
+    feature_vector_len = N_MFCC
+    
+    X = samples[:,0:feature_vector_len]
+    y = samples[:,-1]
+    
+    return X, y
 
 
 def get_time_log(log_filename):
