ebf36291e6745532a3b0ab81f80d3807c6df00fb
James Perks <jamesfperks@aol.com>
2024-01-29 21:30:56 +0000

    improved accuray of labelling by incorporating n_FFT//2 offset. 75% achieved on STFT (72% before)

--- marine_acoustics/data_processing/label.py
+++ marine_acoustics/data_processing/label.py
@@ -12,10 +12,10 @@ def label_features(y_features, logs, sr_default):
     """Label feature vectors as 1 for 'whale' or 0 for 'background'."""
     
     # Get the sample indexes of start/end of whale calls
-    time_indexes = get_time_indexes(logs, sr_default)
+    call_indexes = get_call_indexes(logs, sr_default)
     
-    # Convert time indexes to frame indexes
-    frame_indexes = index2frame(time_indexes)
+    # Convert call indexes to frame indexes
+    frame_indexes = index2frame(call_indexes)
     
     # For each annotation, label corresponding frames 1 if "whale", else 0 
     feature_labels = np.zeros(y_features.shape[0])  
@@ -29,35 +29,42 @@ def label_features(y_features, logs, sr_default):
     return y_labelled_features
 
 
-def index2frame(time_indexes):
+def get_call_indexes(logs, sr_default):
+    """Read the call log time stamps and calculate the index of the start
+    and end of each call for the resampled audio."""
+    
+    original_audio_indexes = logs[['Beg File Samp (samples)',
+                                   'End File Samp (samples)']].to_numpy() - 1
+    
+    call_indexes = np.rint(s.SR*original_audio_indexes/sr_default)
+    
+    return call_indexes
+
+
+def index2frame(call_indexes):
     """Convert time log indexes to frame indexes."""
     
-    # Frame index of the last frame that the sample is in
-    frame_indexes = np.apply_along_axis(librosa.samples_to_frames,
-                                        axis=0,
-                                        arr=time_indexes,
-                                        hop_length=s.HOP_LENGTH,
-                                        n_fft=s.FRAME_LENGTH)
+    # # Frame index of the last frame that the sample is in
+    # frame_indexes = np.apply_along_axis(librosa.samples_to_frames,
+    #                                     axis=0,
+    #                                     arr=call_indexes,
+    #                                     hop_length=s.HOP_LENGTH,
+    #                                     n_fft=s.FRAME_LENGTH)
     
-    # Deal with negative indexes caused by librosa n_fft offset
-    frame_indexes[frame_indexes<0] = 0
+  
+    offset = int(s.FRAME_LENGTH // 2)
+
+    frame_indexes = np.asarray(np.floor((call_indexes + offset)//s.HOP_LENGTH),
+                               dtype=int)
+
+    # # Deal with negative indexes caused by librosa n_fft offset
+    # frame_indexes[frame_indexes<0] = 0
     
-    # Check
-    for idx in frame_indexes.flatten():
-        if idx < 0:
-            raise ValueError('Negative frame index calculated during sample '
-                             'index to frame index conversion.')
+    # # Check
+    # for idx in frame_indexes.flatten():
+    #     if idx < 0:
+    #         raise ValueError('Negative frame index calculated during sample '
+    #                          'index to frame index conversion.')
     
     return frame_indexes
 
-
-def get_time_indexes(logs, sr_default):
-    """Read the call log time stamps and calculate the index of each call
-    for the resampled audio."""
-    
-    unsampled_indexes = logs[['Beg File Samp (samples)',
-                                   'End File Samp (samples)']].to_numpy() - 1
-    
-    time_indexes = np.rint(s.SR*unsampled_indexes/sr_default)
-    
-    return time_indexes
