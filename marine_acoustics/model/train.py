0700bd6992f1abb3747fc00e9fa95ce40e2a7d4d
James Perks <jamesfperks@aol.com>
2024-02-08 10:57:30 +0000

    Use random seed during model training for repeatable results.

--- marine_acoustics/model/train.py
+++ marine_acoustics/model/train.py
@@ -5,6 +5,7 @@ Train the model.
 
 import time
 from sklearn.ensemble import HistGradientBoostingClassifier
+from marine_acoustics.configuration import settings as s
 
 
 def train_classifier(X_train, y_train):
@@ -13,7 +14,8 @@ def train_classifier(X_train, y_train):
     print('  - Training model...', end='')
     
     start = time.time()
-    clf = HistGradientBoostingClassifier().fit(X_train, y_train)
+    clf = HistGradientBoostingClassifier(random_state=s.SEED).fit(X_train,
+                                                                  y_train)
     end = time.time()
     
     print(f'100% ({end-start:.1f} s)')
