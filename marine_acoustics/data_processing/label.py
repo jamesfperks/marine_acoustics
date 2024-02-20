da3f81283d325f4bdb7a232526d59b2fba3b5d7c
James Perks <jamesfperks@aol.com>
2024-02-20 19:37:58 +0000

    changes to labelling to fix index error when using overlapping frames.

--- marine_acoustics/data_processing/label.py
+++ marine_acoustics/data_processing/label.py
@@ -20,7 +20,7 @@ def label_features(y_features, logs, sr_default):
     # For each annotation, label corresponding frames 1 if "whale", else 0 
     feature_labels = np.zeros(y_features.shape[0])  
     for start, end in frame_indexes:  
-        whale_indexes = np.arange(start, end) # Non-inclusive end frame idx
+        whale_indexes = np.arange(start, end+1) # Non-inclusive end frame idx
         feature_labels[whale_indexes] = np.ones(len(whale_indexes))
     
     # Append label to the end of the feature vector for each sample
@@ -34,22 +34,33 @@ def get_call_indexes(logs, sr_default):
     and end of each call for the resampled audio."""
     
     original_audio_indexes = logs[['Beg File Samp (samples)',
-                                   'End File Samp (samples)']].to_numpy() - 1
+                                   'End File Samp (samples)']].to_numpy()
     
-    call_indexes = np.rint(s.SR*original_audio_indexes/sr_default)
+    call_indexes = np.rint(s.SR*original_audio_indexes/sr_default) - 1
     
     return call_indexes
 
 
 def index2frame(call_indexes):
-    """Convert call start and end indexes to frame indexes."""
+    """Convert call start and end indexes to frame indexes.
     
-    # Offset to account for frame centering in feature extraction
+    Overlapping frames cause an index to appear in multiple frames.
+    The start frame index is the last frame in which the start index appears.
+    The end frame index is the first frame in which the end index appears.
+    
+    """
+    
+    # Start frame idx given by the last frame a sample idx is in
+    call_start_idxs = call_indexes[:,0]
     offset = int(s.FRAME_LENGTH // 2)
+    start_frame_idxs = np.asarray((call_start_idxs+offset)//s.HOP_LENGTH,
+                                 dtype=int)
 
-    frame_indexes = np.asarray(np.floor((call_indexes + offset)//s.HOP_LENGTH),
-                               dtype=int)
-
+    # End frame idx given by the first frame a sample idx is in
+    call_end_idxs = call_indexes[:,1]
+    end_frame_idxs = np.asarray(call_end_idxs//s.HOP_LENGTH, dtype=int)
     
+    frame_indexes = np.column_stack((start_frame_idxs, end_frame_idxs))
+
     return frame_indexes
 
